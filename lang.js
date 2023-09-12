75f8004aa0ebaa24804a72923d9864e07d64b2cd
Chanh Tran <minhchanhbkit@gmail.com>
2023-09-12 23:56:04 +0900

    feat: language

---
+++ lang.js
@@ -0,0 +1,44 @@
+var langTexts = {
+  VI: {
+    date: "16/09/2023",
+    sections: {
+      home: "Trang Chủ",
+      couple: "Chúng Tôi",
+      event: "Tiệc Cưới",
+      cheer: "Chung Vui",
+      moments: "Khoảnh Khắc",
+    },
+    couple: {
+      groom: "CHÚ RỂ",
+      bride: "CÔ DÂU",
+    },
+    event: {
+      descriptions: "Tiệc Cưới Của Chúng Tôi Sẽ Tổ Chức Tại",
+    },
+    cheer: {
+      invitation:
+        "Sự hiện diện của quý vị và người thân là niềm hạnh phúc của gia đình chúng tôi. Vui lòng thông báo cho gia đình chúng tôi sự hiện diện của quý vị để chúng tôi có thể tiếp đón chu đáo.",
+    },
+  },
+  EN: {
+    date: "16 Sep 2023",
+    sections: {
+      home: "Home",
+      couple: "Our Couple",
+      event: "Wedding Event",
+      cheer: "Cheers to Our Love",
+      moments: "Our Selected Moments",
+    },
+    couple: {
+      groom: "THE GROOM",
+      bride: "THE BRIDE",
+    },
+    event: {
+      descriptions: "Our Wedding Party Will Be Held At",
+    },
+    cheer: {
+      invitation:
+        "We are honored and greateful to have you and your love ones at our wedding party. Please let us know whether you could come and cheer to our happiness.",
+    },
+  },
+};
